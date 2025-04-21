5c2c7b3e2f6d362f03516dea375bdc34c42f579f
Qwor01 <nachoesposito@gmail.com>
2025-04-21 10:10:57 +0200

    build: Added the shared dictionary

--- telegram_bot/message_builder.py
+++ telegram_bot/message_builder.py
@@ -1,10 +1,16 @@
 
 
-def build_message():
+def build_message(joined):
     message='''
 QUEDADA JUEGOMESEO\n
 INICIO: 19:30\n
 FIN: 23:00\n
-PARTICIPANTES:\n
-            '''
+PARTICIPANTES:\n'''
+    for user_id, info in joined.items():
+        name = info["name"]
+        guests = info["guests"]
+        if guests > 0:
+            message += f"-@{name} (+{guests})\n"
+        else:
+            message += f"-@{name}\n"
     return message
