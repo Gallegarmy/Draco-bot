a7a54b688cb28946444bbc57b9787afd2d802db0
Qwor01 <nachoesposito@gmail.com>
2025-04-21 10:11:21 +0200

    build: Added the shared dictionary

--- telegram_bot/quedada_entry.py
+++ telegram_bot/quedada_entry.py
@@ -1,13 +1,17 @@
 from telegram import Update,  InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
-import structlog
-logger = structlog.get_logger()
+from utils.logger import logger
 from .message_builder import build_message
 from .keyboard_builder import build_keyboard
+from shared import joined
 
 async def quedada(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+    user_id = user.id
+    #Add the user that created the event to the dict
+    joined[user_id] = {"name": user.username if user.username else user.full_name, "guests": 0}
     logger.info("Building quedada message")
-    message = build_message()
+    message = build_message(joined)
     reply_markup = InlineKeyboardMarkup(build_keyboard())
     logger.info("Quedada message built")
     await update.message.reply_text(message, reply_markup=reply_markup)
