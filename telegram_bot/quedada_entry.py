f2348821de60204b64f6a0d11cbfd3d327646306
Qwor01 <nachoesposito@gmail.com>
2025-04-22 11:56:22 +0200

    change: Changed the message and keyboard invoked to work with the new keyboards

--- telegram_bot/quedada_entry.py
+++ telegram_bot/quedada_entry.py
@@ -1,8 +1,8 @@
 from telegram import Update,  InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 from utils.logger import logger
-from .message_builder import build_message
-from .keyboard_builder import build_keyboard
+from .final_message_builder import build_final_message
+from .keyboards.start_time_keyboard_builder import build_start_keyboard
 from shared import joined
 
 async def quedada(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -11,8 +11,8 @@ async def quedada(update: Update, context: ContextTypes.DEFAULT_TYPE):
     #Add the user that created the event to the dict
     joined[user_id] = {"name": user.username if user.username else user.full_name, "guests": 0}
     logger.info("Building quedada message")
-    message = build_message(joined)
-    reply_markup = InlineKeyboardMarkup(build_keyboard())
-    logger.info("Quedada message built")
+    #Starts the routin that will ask for start time, end time and status
+    message = "Indique la hora de inicio"
+    reply_markup = InlineKeyboardMarkup(build_start_keyboard())
     await update.message.reply_text(message, reply_markup=reply_markup)
-    logger.info("Quedada message sent")
+    logger.info("Start time message sent")
