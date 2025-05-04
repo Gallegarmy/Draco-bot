c0bca9b1b195f990ef6debfe3438d475ba87dcfd
Qwor01 <nachoesposito@gmail.com>
2025-05-04 22:55:18 +0200

    change: Moved the entry method to ask for a meeting name

--- telegram_bot/quedada_entry.py
+++ telegram_bot/quedada_entry.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from utils.logger import logger
-from .constants import ENTER_START_TIME
+from .constants import MEETING_NAME
 from .calendar.telegramcalendar import create_calendar
 from .utils import get_username
 
@@ -12,6 +12,8 @@ async def quedada(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.chat_data["current_event_id"] = str(event_id)
     context.chat_data["current"] = {
         "creator_id": update.message.from_user.id,
+        "meeting_name": None,
+        "meeting_description": None,
         "start_date": None,
         "start_time": None,
         "meeting_type": "Open",
@@ -21,8 +23,5 @@ async def quedada(update: Update, context: ContextTypes.DEFAULT_TYPE):
     }
     logger.info(f"Building quedada message: message id {event_id}")
     #Starts the routin that will ask for start time, end time and status
-    message = "Indique la fecha de inicio"
-    reply_markup = create_calendar("start_date")
-    await update.message.reply_text(message, reply_markup=reply_markup)
-    logger.info("Start time keyboard shown")
-    return ENTER_START_TIME
+    await update.message.reply_text("¿Cuál es el nombre de la quedada?")
+    return MEETING_NAME
